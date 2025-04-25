f9abc6efd790d0ada218162077b20a7751842d52
Adrian Stanisz <adi.stanisz@gmail.com>
2025-04-25 11:21:51 +0200

    zadanie_07.1

--- Plat-dot-NET-32INF-SSI-NP_LAB_03_Adrian_Stanisz/Zadanie_07/Program.cs
+++ Plat-dot-NET-32INF-SSI-NP_LAB_03_Adrian_Stanisz/Zadanie_07/Program.cs
@@ -6,24 +6,5 @@ class Program
 {
     static void Main(string[] args)
     {
-       Sentence sample = new Sentence();
-       sample.text = "Ala ma kota ale kot nie ma ali";
-       
-       List<char> letters = new List<char>();
-       letters.Add('a');
-       letters.Add('e');
-       letters.Add('i');
-       letters.Add('o');
-       letters.Add('u');
-       letters.Add('y');
-
-       foreach (char c in sample)
-       {
-           if (letters.Contains(char.ToLower(c)))
-           {
-               Console.WriteLine(c);
-           }
-       }
-
     }
 }
